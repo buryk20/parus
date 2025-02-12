b1b58eca787c5acbf664025c0b2619c9253f9107
buryk20 <burykseriy@gmail.com>
2025-02-12 21:41:53 +0200

    footer mob

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -2,34 +2,38 @@
     <div class="footer__wrp">
         <div class="footer__wrp-cont">
             <img src="/public/img/footer-logo.svg" alt="footer logo">
-            <div class="footer__nav">
-                <a class="footer__nav-item" href="#about-project">Про комплекс</a>
-                <a class="footer__nav-item" href="#why-choose">Переваги</a>
-                <a class="footer__nav-item" href="#planning">Планування</a>
-                <a class="footer__nav-item" href="#">Інфраструктура та комфорт</a>
-                <a class="footer__nav-item" href="#life">Галерея</a>
-                <a class="footer__nav-item" href="#">Консультація
-                </a>
+            <div class="footer__wrp-info">
+                <div class="footer__nav">
+                    <a class="footer__nav-item" href="#about-project">Про комплекс</a>
+                    <a class="footer__nav-item" href="#why-choose">Переваги</a>
+                    <a class="footer__nav-item" href="#planning">Планування</a>
+                    <a class="footer__nav-item" href="#">Інфраструктура та комфорт</a>
+                    <a class="footer__nav-item" href="#life">Галерея</a>
+                    <a class="footer__nav-item" href="#">Консультація
+                    </a>
+                </div>
+                <div class="footer__social-wrp">
+                    <a href="#">
+                        <img src="/public/icon/footer-inst.svg" alt="instagram">
+                    </a>
+                    <a href="#">
+                        <img src="/public/icon/footer-facebook.svg" alt="facebook">
+                    </a>
+                    <a href="#">
+                        <img src="/public/icon/footer-youtube.svg" alt="youtube">
+                    </a>
+                </div>
+                <div class="footer__map-wrp">
+                    <p class="footer__map-text">
+                        м. Южне,
+                    </p>
+                    <p class="footer__map-text">
+                        вул. Приморська, 1
+                    </p>
+                    <a class="footer__tel" href="tel:+0 800 757807">0 800 757 807</a>
+                </div>
             </div>
-            <div class="footer__social-wrp">
-                <a href="#">
-                    <img src="/public/icon/footer-inst.svg" alt="instagram">
-                </a>
-                <a href="#">
-                    <img src="/public/icon/footer-facebook.svg" alt="facebook">
-                </a>
-                <a href="#">
-                    <img src="/public/icon/footer-youtube.svg" alt="youtube">
-                </a>
-            </div>
-            <div class="footer__map-wrp">
-                <p>
-                    м. Южне,
-                </p>
-                <p>
-                    вул. Приморська, 1
-                </p>
-            </div>
+
         </div>
     </div>
 </footer>
